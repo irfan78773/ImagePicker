103e8b911e57ad498d08c15fb229551f2c69a200
irfan78773 <45169805+irfan78773@users.noreply.github.com>
2019-07-23 19:00:22 +0530

    Add files via upload
    Use :
     ImagePickerManager().pickImage(self){ image in
               self.yourimageName.image = image
    }

---
+++ imagePicker.swift
@@ -0,0 +1,85 @@
+//
+//  imagePicker.swift
+//  SamplePicker
+//
+//  Created by Pixster Studio on 23/07/19.
+//  Copyright © 2019 irfanpathan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ImagePickerManager: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var picker = UIImagePickerController();
+    var alert = UIAlertController(title: "Select Image", message: nil, preferredStyle: .actionSheet)
+    var viewController: UIViewController?
+    var pickImageCallback : ((UIImage) -> ())?;
+    
+    override init(){
+        super.init()
+    }
+    
+    func pickImage(_ viewController: UIViewController, _ callback: @escaping ((UIImage) -> ())) {
+        pickImageCallback = callback;
+        self.viewController = viewController;
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default){
+            UIAlertAction in
+            self.openCamera()
+        }
+        let galleryAction = UIAlertAction(title: "Gallery", style: .default){
+            UIAlertAction in
+            self.openGallery()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel){
+            UIAlertAction in
+        }
+        
+        // Add the actions
+        picker.delegate = self
+        alert.addAction(cameraAction)
+        alert.addAction(galleryAction)
+        alert.addAction(cancelAction)
+        alert.popoverPresentationController?.sourceView = self.viewController!.view
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera(){
+        alert.dismiss(animated: true, completion: nil)
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            self.viewController!.present(picker, animated: true, completion: nil)
+        } else {
+            let alertWarning = UIAlertView(title:"Alert", message: "camera not available", delegate:nil, cancelButtonTitle:"OK", otherButtonTitles:"")
+            alertWarning.show()
+        }
+    }
+    func openGallery(){
+        alert.dismiss(animated: true, completion: nil)
+        picker.sourceType = .photoLibrary
+        self.viewController!.present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+   
+    
+      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+          picker.dismiss(animated: true, completion: nil)
+          guard let image = info[.originalImage] as? UIImage else {
+              fatalError("Dictionary: \(info)")
+          }
+          pickImageCallback?(image)
+      }
+    
+    
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, pickedImage: UIImage?) {
+    }
+    
+}
